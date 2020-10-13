b4b9baec33e06f458449dbdff33c3aa61db6d3e4
Subhajit Nandi <subhajit.nandi@innovaccer.com>
2020-10-13 14:52:40 +0530

    fix: cors headers for socketio

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ const server = app.listen(PORT, () => {
 	console.log(`serving on port ${PORT}`)
 })
 
-const io = socketio(server)
+const io = socketio(server, {
+	handlePreflightRequest: (req, res) => {
+		const headers = {
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+			'Access-Control-Allow-Origin': req.headers.origin, //or the specific origin you want to give access to,
+			'Access-Control-Allow-Credentials': true
+		}
+		res.writeHead(200, headers)
+		res.end()
+	}
+})
 
 io.on('connection', (socket) => {
 	let editorId = null
